b237291f37f611f837edd72d23f71f444f07518b
hamzahassan54 <documentarytvofficial007@gmail.com>
2022-05-18 20:29:53 +0500

    fdfds

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   http_timeout: 150, //unit: seconds, 0: no timeout
-  apiUrl: "https://scalio.herokuapp.com/"
+  apiUrl: "https://scalioteck.herokuapp.com/"
 };
